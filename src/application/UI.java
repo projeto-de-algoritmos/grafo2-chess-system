814c9b296db74b7168381095a6359ce8f9cb4933
MacLovin <monteiro9961@gmail.com>
2022-12-03 04:12:52 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -11,7 +11,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println(" a b c d e f g h");
+		System.out.println("a b c d e f g h");
 	}
 	
 	public static void printPiece(ChessPiece piece) {
